df7539cbc45f676c145fcc3ae110abbdbb78c7ea
Juhani Toivonen <juhani.toivonen@cs.helsinki.fi>
2012-02-02 12:45:48 +0200

    NetBeans projektin luominen

---
+++ game_of_life/src/game_of_life/Game_of_life.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game_of_life;
+
+/**
+ *
+ * @author 
+ */
+public class Game_of_life {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
